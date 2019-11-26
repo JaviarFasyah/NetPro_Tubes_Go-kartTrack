8521a9bf701667a9a3e47775c8900b42eb27d447
JaviarFasyah <fasyahj@yahoo.com>
2019-11-27 03:06:01 +0700

    11/27/2019_3:05

--- gokt.go
+++ gokt.go
@@ -40,9 +40,9 @@ func (c *conf) readconf(i string) *conf {
 func help() {
 	fmt.Println("Go-kart Track Web Server v1.0")
 	fmt.Println("    This web server will use parameter input, configuration file (.yaml), or hard-coded configuration (if")
-	fmt.Println("no parameter input nor configuration file given) to run. To run with parameter input, use the followings:")
+	fmt.Println("no parameter input or configuration file given) to run. To run with parameter input, use the followings:")
 	fmt.Println("    > '-conf' : The configuration file used by this server (.yaml)")
-	fmt.Println("    > '-http' : The protocol used by this server ('1' for HTTP/1.1, '2' for HTTP/2, 's' for HTTPS)")
+	fmt.Println("    > '-http' : The protocol used by this server ('1' for HTTP/1.1 and 's'/'S' for HTTPS/2)")
 	fmt.Println("    > '-port' : The port number used by this server")
 	fmt.Println("    > '-dir'  : The server root directory used by this server")
 	fmt.Println("    > '-cert' : The certificate file used by this server (HTTPS protocol)")
@@ -68,11 +68,11 @@ func servehttp1(i string, j conf) {
 			lcnf.Port = dcnf.Port
 		}
 	}
-	if lcnf.Dir == "" {
-		if d.Dir != "" {
-			lcnf.Dir = d.Dir
-		} else {
+	if lcnf.Dir == "" || lcnf.Dir == "." {
+		if d.Dir == "" || d.Dir == "." {
 			lcnf.Dir = dcnf.Dir
+		} else {
+			lcnf.Dir = d.Dir
 		}
 	}
 	fs := http.FileServer(http.Dir(lcnf.Dir))
@@ -80,8 +80,6 @@ func servehttp1(i string, j conf) {
 	fmt.Println("running on HTTP/1.1 |", "server directory:", lcnf.Dir, "| port:", lcnf.Port)
 	http.ListenAndServe(":"+strconv.Itoa(lcnf.Port), nil)
 }
-
-func servehttp2() {}
 
 func servehttps(i string, j conf) {
 	var c conf
@@ -100,11 +98,11 @@ func servehttps(i string, j conf) {
 			lcnf.Port = dcnf.Port
 		}
 	}
-	if lcnf.Dir == "" {
-		if d.Dir != "" {
-			lcnf.Dir = d.Dir
-		} else {
+	if lcnf.Dir == "" || lcnf.Dir == "." {
+		if d.Dir == "" || d.Dir == "." {
 			lcnf.Dir = dcnf.Dir
+		} else {
+			lcnf.Dir = d.Dir
 		}
 	}
 	if lcnf.Cert == "" {
@@ -123,15 +121,17 @@ func servehttps(i string, j conf) {
 	}
 	fs := http.FileServer(http.Dir(lcnf.Dir))
 	http.Handle("/", fs)
-	fmt.Println("running on HTTPS |", "server directory:", lcnf.Dir, "| port:", lcnf.Port)
+	fmt.Println("running on HTTPS/2 |", "server directory:", lcnf.Dir, "| port:", lcnf.Port)
 	http.ListenAndServeTLS(":"+strconv.Itoa(lcnf.Port), lcnf.Cert, lcnf.Key, nil)
+	fmt.Printf("error: unable to find %v and/or %v file to run HTTPS/2", lcnf.Cert, lcnf.Key)
 }
 
 func main() {
 	var rcnf conf
+	var bcnf *conf
 	fhelp := flag.Bool("help", false, "help")
 	fconf := flag.String("conf", "", "The configuration file used by this server (.yaml)")
-	fhttp := flag.String("http", "", "The protocol used by this server ('1' for HTTP/1.1, '2' for HTTP/2, 's' for HTTPS)")
+	fhttp := flag.String("http", "", "The protocol used by this server ('1' for HTTP/1.1 and 's'/'S' for HTTPS/2)")
 	fport := flag.Int("port", 0, "The port number used by this server")
 	fdir := flag.String("dir", "", "The server root directory used by this server")
 	fcert := flag.String("cert", "", "The certificate file used by this server (HTTPS protocol)")
@@ -150,32 +150,57 @@ func main() {
 			servehttp1(*fconf, fcnf)
 		} else if *fhttp == "s" || *fhttp == "S" {
 			servehttps(*fconf, fcnf)
-		} else if *fhttp == "2" {
-			servehttp2()
 		} else {
-			servehttp1(dconf, fcnf)
+			fmt.Printf("error: unknown protocol %v and %v file not found, switch using default configuration\n", *fhttp, *fconf)
+			bcnf = rcnf.readconf(dconf)
+			if bcnf.Http == "1" {
+				servehttp1(dconf, fcnf)
+			} else if bcnf.Http == "s" || bcnf.Http == "S" {
+				servehttps(dconf, fcnf)
+			} else {
+				servehttp1(dconf, fcnf)
+			}
 		}
 	} else if *fhttp == "" && *fconf != "" {
-		if rcnf.readconf(*fconf).Http == "1" {
+		bcnf = rcnf.readconf(*fconf)
+		if bcnf.Http == "1" {
 			servehttp1(*fconf, fcnf)
-		} else if rcnf.readconf(*fconf).Http == "s" || rcnf.readconf(*fconf).Http == "S" {
+		} else if bcnf.Http == "s" || bcnf.Http == "S" {
 			servehttps(*fconf, fcnf)
-		} else if rcnf.readconf(*fconf).Http == "2" {
-			servehttp2()
 		} else {
-			servehttp1(dconf, fcnf)
+			bcnf = rcnf.readconf(dconf)
+			if bcnf.Http == "1" {
+				servehttp1(dconf, fcnf)
+			} else if bcnf.Http == "s" || bcnf.Http == "S" {
+				servehttps(dconf, fcnf)
+			} else {
+				servehttp1(dconf, fcnf)
+			}
 		}
 	} else if *fhttp != "" && *fconf == "" {
 		if *fhttp == "1" {
 			servehttp1(dconf, fcnf)
 		} else if *fhttp == "s" || *fhttp == "S" {
 			servehttps(dconf, fcnf)
-		} else if *fhttp == "2" {
-			servehttp2()
+		} else {
+			fmt.Printf("error: unknown protocol %v, switch using default configuration\n", *fhttp)
+			bcnf = rcnf.readconf(dconf)
+			if bcnf.Http == "1" {
+				servehttp1(dconf, fcnf)
+			} else if bcnf.Http == "s" || bcnf.Http == "S" {
+				servehttps(dconf, fcnf)
+			} else {
+				servehttp1(dconf, fcnf)
+			}
+		}
+	} else if *fhttp == "" && *fconf == "" {
+		bcnf = rcnf.readconf(dconf)
+		if bcnf.Http == "1" {
+			servehttp1(dconf, fcnf)
+		} else if bcnf.Http == "s" || bcnf.Http == "S" {
+			servehttps(dconf, fcnf)
 		} else {
 			servehttp1(dconf, fcnf)
 		}
-	} else if *fhttp == "" && *fconf == "" {
-		servehttp1(dconf, fcnf)
 	}
 }
