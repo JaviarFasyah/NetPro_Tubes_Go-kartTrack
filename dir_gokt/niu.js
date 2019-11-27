4f3255df5c20d20ca9788f6cb817a45d1a2f0a61
JaviarFasyah <fasyahj@yahoo.com>
2019-11-28 02:35:42 +0700

    11/28/2019_02:35

---
+++ dir_gokt/niu.js
@@ -0,0 +1,11 @@
+function btn1() {
+    document.getElementById("btn1").style.display="none";
+    document.getElementById("btn2").style.display="inline";
+    document.getElementById("p1").innerHTML="Klik tombol salmon"
+}
+
+function btn2(){
+    document.getElementById("btn2").style.display="none";
+    document.getElementById("btn1").style.display="inline";
+    document.getElementById("p1").innerHTML="Klik tombol biru"
+}
